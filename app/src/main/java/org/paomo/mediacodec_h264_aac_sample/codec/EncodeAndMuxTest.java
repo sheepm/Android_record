04d965c489c72856c4b74373dcb8e423a7c62104
paomo <redswing@foxmail.com>
2016-12-26 20:21:30 +0800

    encoder

--- app/src/main/java/org/paomo/mediacodec_h264_aac_sample/codec/EncodeAndMuxTest.java
+++ app/src/main/java/org/paomo/mediacodec_h264_aac_sample/codec/EncodeAndMuxTest.java
@@ -10,12 +10,14 @@ import android.opengl.EGLContext;
 import android.opengl.EGLDisplay;
 import android.opengl.EGLExt;
 import android.opengl.EGLSurface;
+import android.opengl.GLES20;
 import android.os.Environment;
 import android.test.AndroidTestCase;
 import android.view.Surface;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * Created by Administrator on 2016/12/24 0024.
@@ -55,11 +57,32 @@ public class EncodeAndMuxTest extends AndroidTestCase {
     /**
      * encode AAC video from a surface,the output is saved as an mp4 file
      */
-    public void encodeVideoToMp4(){
+    public void encodeVideoToMp4() {
         //QVGA  2Mbps
         mWidth = 320;
         mHeight = 240;
         mBitRate = 2000000;
+
+        try {
+            prepareEncoder();
+            mInputSurface.makeCurrent();
+
+            for (int i = 0; i < NUM_FRAMS; i++) {
+                //feed pending encoder output into the muxer
+                drainEncoder(false);
+
+                //generate a new frame of input
+                generateSurfaceFrame(i);
+
+                mInputSurface.setPresentationTime(computePresentationTimeNesc(i));
+                mInputSurface.swapBuffers();
+            }
+            drainEncoder(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            releaseEncoder();
+        }
     }
 
     /**
@@ -67,26 +90,130 @@ public class EncodeAndMuxTest extends AndroidTestCase {
      */
     private void prepareEncoder() throws IOException {
         mBufferInfo = new MediaCodec.BufferInfo();
-        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE,mWidth,mHeight);
+        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
 
         format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
-        format.setInteger(MediaFormat.KEY_BIT_RATE,mBitRate);
-        format.setInteger(MediaFormat.KEY_FRAME_RATE,FRAME_RATE);
-        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL,IFRAME_INTERVAL);
+        format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
 
         mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
-        mEncoder.configure(format,null,null,MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
         mInputSurface = new CodecInputSurface(mEncoder.createInputSurface());
         mEncoder.start();
 
-        String outputPath = new File(OUTPUT_DIR,"test." + mWidth + "x" + mHeight + ".mp4").toString();
+        String outputPath = new File(OUTPUT_DIR, "test." + mWidth + "x" + mHeight + ".mp4").toString();
 
-        mMuxer = new MediaMuxer(outputPath,MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+        mMuxer = new MediaMuxer(outputPath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
 
         mTrackIndex = -1;
         mMuxerStarted = false;
     }
 
+    private void drainEncoder(boolean endOfStream) {
+        final int TIMEOUT_USEC = 10000;
+        if (endOfStream) {
+            mEncoder.signalEndOfInputStream();
+        }
+        ByteBuffer[] encoderOutputBuffers = mEncoder.getOutputBuffers();
+        while (true) {
+            int encoderStatus = mEncoder.dequeueOutputBuffer(mBufferInfo, TIMEOUT_USEC);
+            if (encoderStatus == MediaCodec.INFO_TRY_AGAIN_LATER) {
+                break;
+            } else if (encoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
+                encoderOutputBuffers = mEncoder.getOutputBuffers();
+            } else if (encoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                if (mMuxerStarted) {
+                    throw new RuntimeException("format change twice");
+                }
+                MediaFormat newFormat = mEncoder.getOutputFormat();
+
+                //now that we have the magic goodies , start the muxer
+                mTrackIndex = mMuxer.addTrack(newFormat);
+                mMuxer.start();
+                mMuxerStarted = true;
+            } else if (encoderStatus < 0) {
+                //some unexpected result from encoder.dequeueOutputBuffer
+                //ignore it
+            } else {
+                ByteBuffer encodeData = encoderOutputBuffers[encoderStatus];
+                if (encodeData == null) {
+                    throw new RuntimeException("encoderOutputBuffer " + encoderStatus + " was null");
+                }
+                if ((mBufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+                    mBufferInfo.size = 0;
+                }
+                if (mBufferInfo.size != 0) {
+                    if (!mMuxerStarted) {
+                        throw new RuntimeException("muxer not start");
+                    }
+                    encodeData.position(mBufferInfo.offset);
+                    encodeData.limit(mBufferInfo.offset + mBufferInfo.size);
+
+                    mMuxer.writeSampleData(mTrackIndex, encodeData, mBufferInfo);
+                }
+                mEncoder.releaseOutputBuffer(encoderStatus, false);
+
+                if ((mBufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+                    if (!endOfStream) {
+                        //reached end of stream unexpected
+                    } else {
+                        //end of stream reached
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    private void generateSurfaceFrame(int frameIndex) {
+        frameIndex %= 8;
+
+        int startX, startY;
+        if (frameIndex < 4) {
+            //(0,0) is bottom-left in GL
+            startX = frameIndex * (mWidth / 4);
+            startY = mHeight / 2;
+        } else {
+            startX = (7 - frameIndex) * (mWidth / 4);
+            startY = 0;
+        }
+        GLES20.glClearColor(TEST_R0 / 255.0f, TEST_G0 / 255.0f, TEST_B0 / 255.0f, 1.0f);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        GLES20.glEnable(GLES20.GL_SCISSOR_TEST);
+        GLES20.glScissor(startX, startY, mWidth / 4, mHeight / 2);
+        GLES20.glClearColor(TEST_R1 / 255.0f, TEST_G1 / 255.0f, TEST_B1 / 255.0f, 1.0f);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glDisable(GLES20.GL_SCISSOR_TEST);
+    }
+
+    /**
+     * nanosecond
+     * @param frameIndex
+     * @return
+     */
+    private long computePresentationTimeNesc(int frameIndex){
+        final long ONE_BILLION = 1000000000;
+        return frameIndex * ONE_BILLION / FRAME_RATE;
+    }
+
+    private void releaseEncoder() {
+        if (mEncoder != null) {
+            mEncoder.stop();
+            mEncoder.release();
+            mEncoder = null;
+        }
+        if (mInputSurface != null) {
+            mInputSurface.release();
+            mInputSurface = null;
+        }
+        if (mMuxer != null) {
+            mMuxer.stop();
+            mMuxer.release();
+            mMuxer = null;
+        }
+    }
 
     private static class CodecInputSurface {
         private static final int EGL_RECORDABLE_ANDROID = 0x3142;
@@ -126,28 +253,28 @@ public class EncodeAndMuxTest extends AndroidTestCase {
             };
             EGLConfig[] configs = new EGLConfig[1];
             int[] numConfigs = new int[1];
-            EGL14.eglChooseConfig(mEGLDisplay,attribList,0,configs,0,configs.length,numConfigs,0);
+            EGL14.eglChooseConfig(mEGLDisplay, attribList, 0, configs, 0, configs.length, numConfigs, 0);
             checkEglError("eglCreateContext RGB888 + recordable ES2");
             //Configure Context for OpenGL ES 2.0
             int[] attrib_list = {
-                    EGL14.EGL_CONTEXT_CLIENT_VERSION,2,
+                    EGL14.EGL_CONTEXT_CLIENT_VERSION, 2,
                     EGL14.EGL_NONE
             };
-            mEGLContext = EGL14.eglCreateContext(mEGLDisplay,configs[0],EGL14.EGL_NO_CONTEXT,attrib_list,0);
+            mEGLContext = EGL14.eglCreateContext(mEGLDisplay, configs[0], EGL14.EGL_NO_CONTEXT, attrib_list, 0);
             checkEglError("eglCreateContext");
             //Create a window surface ,attach it to the surface received
             int[] surfaceAttribs = {
                     EGL14.EGL_NONE
             };
-            mEGLSurface = EGL14.eglCreateWindowSurface(mEGLDisplay,configs[0],mSurface,surfaceAttribs,0);
+            mEGLSurface = EGL14.eglCreateWindowSurface(mEGLDisplay, configs[0], mSurface, surfaceAttribs, 0);
             checkEglError("eglCreateWindowSurface");
         }
 
-        public void release(){
-            if (mEGLDisplay != EGL14.EGL_NO_DISPLAY){
-                EGL14.eglMakeCurrent(mEGLDisplay,EGL14.EGL_NO_SURFACE,EGL14.EGL_NO_SURFACE,EGL14.EGL_NO_CONTEXT);
-                EGL14.eglDestroySurface(mEGLDisplay,mEGLSurface);
-                EGL14.eglDestroyContext(mEGLDisplay,mEGLContext);
+        public void release() {
+            if (mEGLDisplay != EGL14.EGL_NO_DISPLAY) {
+                EGL14.eglMakeCurrent(mEGLDisplay, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_CONTEXT);
+                EGL14.eglDestroySurface(mEGLDisplay, mEGLSurface);
+                EGL14.eglDestroyContext(mEGLDisplay, mEGLContext);
                 EGL14.eglReleaseThread();
                 EGL14.eglTerminate(mEGLDisplay);
             }
@@ -161,13 +288,13 @@ public class EncodeAndMuxTest extends AndroidTestCase {
             mSurface = null;
         }
 
-        public void makeCurrent(){
-            EGL14.eglMakeCurrent(mEGLDisplay,mEGLSurface,mEGLSurface,mEGLContext);
+        public void makeCurrent() {
+            EGL14.eglMakeCurrent(mEGLDisplay, mEGLSurface, mEGLSurface, mEGLContext);
             checkEglError("eglMakeCurrent");
         }
 
-        public boolean swapBuffers(){
-            boolean result = EGL14.eglSwapBuffers(mEGLDisplay,mEGLSurface);
+        public boolean swapBuffers() {
+            boolean result = EGL14.eglSwapBuffers(mEGLDisplay, mEGLSurface);
             checkEglError("eglSwapBuffers");
             return result;
         }
@@ -175,15 +302,15 @@ public class EncodeAndMuxTest extends AndroidTestCase {
         /**
          * send pts to EGL , nanoseconds
          */
-        public void setPresentationTime(long nsecs){
-            EGLExt.eglPresentationTimeANDROID(mEGLDisplay,mEGLSurface,nsecs);
+        public void setPresentationTime(long nsecs) {
+            EGLExt.eglPresentationTimeANDROID(mEGLDisplay, mEGLSurface, nsecs);
             checkEglError("eglPresentationTimeANDROID");
         }
 
-        private void checkEglError(String msg){
+        private void checkEglError(String msg) {
             int error;
-            if ((error = EGL14.eglGetError()) != EGL14.EGL_SUCCESS){
-                throw new RuntimeException(msg + ":EGL ERROR : 0x " +Integer.toHexString(error));
+            if ((error = EGL14.eglGetError()) != EGL14.EGL_SUCCESS) {
+                throw new RuntimeException(msg + ":EGL ERROR : 0x " + Integer.toHexString(error));
             }
         }
     }
