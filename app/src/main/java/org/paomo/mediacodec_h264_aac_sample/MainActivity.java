da0a7f80755b68a6eeae8ab2498d4ab2a0e67bdf
paomo <redswing@foxmail.com>
2018-03-27 10:52:25 +0800

    clear project

--- app/src/main/java/org/paomo/mediacodec_h264_aac_sample/MainActivity.java
+++ app/src/main/java/org/paomo/mediacodec_h264_aac_sample/MainActivity.java
@@ -1,13 +1,77 @@
 package org.paomo.mediacodec_h264_aac_sample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import org.paomo.mediacodec_h264_aac_sample.camera.CameraPreview;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "CameraPreview";
+
+    /**
+     * Id of the camera to access. 0 is the first camera.
+     */
+    private static final int CAMERA_ID = 0;
+
+    private CameraPreview mPreview;
+    private Camera mCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Open an instance of the first camera and retrieve its info.
+        mCamera = getCameraInstance(CAMERA_ID);
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        Camera.getCameraInfo(CAMERA_ID, cameraInfo);
+
+        if (mCamera == null || cameraInfo == null) {
+            // Camera is not available, display error message
+            Toast.makeText(this, "Camera is not available.", Toast.LENGTH_SHORT).show();
+//            setContentView(R.layout.activity_camera_unavailable);
+        } else {
+
+            setContentView(R.layout.activity_main);
+
+            // Get the rotation of the screen to adjust the preview image accordingly.
+            final int displayRotation = getWindowManager().getDefaultDisplay()
+                    .getRotation();
+
+            // Create the Preview view and set it as the content of this Activity.
+            mPreview = new CameraPreview(this, mCamera, cameraInfo, displayRotation);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop camera access
+        releaseCamera();
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    private Camera getCameraInstance(int cameraId) {
+        Camera c = null;
+        try {
+            c = Camera.open(cameraId); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+            Toast.makeText(this, "Camera " + cameraId + " is not available: " + e.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
     }
 }
